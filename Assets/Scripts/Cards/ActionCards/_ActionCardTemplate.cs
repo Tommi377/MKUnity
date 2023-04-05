1d206913a0a5c8134e14af30cbaea3bf6220afa4
Pakarinen Tommi <tommi.pakarinen@aalto.fi>
2023-04-05 19:33:38 +0900

    added fire bolt and ice bolt cards

---
+++ Assets/Scripts/Cards/ActionCards/_ActionCardTemplate.cs
@@ -0,0 +1,13 @@
+
+public class _ActionCardTemplate : ActionCard {
+    public _ActionCardTemplate(ActionCardSO actionCardSO) : base(actionCardSO) { }
+
+    public override void Apply(CardChoice choice) {
+        switch (choice.Id) {
+            case 0:
+                break;
+            case 1:
+                break;
+        }
+    }
+}
